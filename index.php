260fd0a9d2ec376e41bdaa166ea93582492b6667
Iqra Khan <162395214+IqraKhan0209@users.noreply.github.com>
2024-07-15 12:57:10 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>home</title>
+     <!-- swiper css link-->
+     <link  rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+<!--font awesome cdn link-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+<!-- custom css file link-->
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!-- header section starts-->
+     <section class="header">
+        <a href="index.php" class="logo">Tours & Travels</a>
+        <nav class="navbar">
+            <a href="index.php">home</a>
+            <a href="about.php">about</a>
+            <a href="package.php">package</a>
+            <a href="book.php">book</a>
+        </nav>
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+     </section>
+    <!-- header section ends -->
+
+    <!-- home section starts-->
+
+    <section class="home">
+        <div class="swiper home-slider">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide slide" style="background:url(images/home-slide-1.jpg) no-repeat">
+                    <div class="content">
+                        <span>explore, discover, travel</span>
+                        <h3>Travel around the world</h3>
+                        <a href="package.php" class="btn">discover more</a>
+
+                    </div>
+                </div>
+
+
+                <div class="swiper-slide slide" style="background:url(images/home-slide-2.jpg) no-repeat">
+                    <div class="content">
+                        <span>explore, discover, travel</span>
+                        <h3>discover the new places</h3>
+                        <a href="package.php" class="btn">discover more</a>
+
+                    </div>
+                </div>
+
+
+                <div class="swiper-slide slide" style="background:url(images/home-slide-3.jpg) no-repeat">
+                    <div class="content">
+                        <span>explore, discover, travel</span>
+                        <h3>Make your tour worthwhile</h3>
+                        <a href="package.php" class="btn">discover more</a>
+
+                    </div>
+                </div>
+
+            </div>
+
+
+
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+        </div>
+    </section>
+    <!-- home section ends-->
+
+
+<!-- services section starts-->
+<section class="services">
+    <h1 class="heading-title">Our Services</h1>
+    <div class="box-container">
+        <div class="box">
+            <img src="images/icon-1.png" width="50%">
+            <h3>Adventure</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/icon-2.png" width="50%">
+            <h3>Tour Guide</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/icon-3.png" width="50%">
+            <h3>Trekking</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/icon-4.png" width="50%">
+            <h3>Camp Fire</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/icon-5.png" width="50%">
+            <h3>Off Road</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/icon-6.png" width="50%">
+            <h3>Camping</h3>
+        </div>
+    </div>
+</section>
+
+<!-- services section ends--> 
+<!--home about section starts-->
+<section class="home-about">
+    <div class="image">
+        <img src="images/about-img.jpg" width="100%">
+    </div>
+
+    <div class="content">
+        <h3>about us</h3>
+        <p>Nurtured from the seed of a single idea ‘to empower the traveller with easy & instant travel bookings providing comprehensive choices’, Tours & Travels is a pioneer in India’s online travel space. Founded by Deep Kalra in the year 2000, Tours & Travels began its journey by serving the US-India travel market with best-value products & services, powered by robust technology and round-the-clock customer support.
+        After successfully consolidating its position as a customer-first brand, known for its reliability and transparency, Tours & Travels launched its India operations in 2005.
+Over the years, we have partnered with many leading brands from the aviation & hospitality industries, creating fruitful partner relations for business expansion opportunities. We also entered the homestays & villas and continue to procure increased market share market in the same. With this, we also entered the ground transport space and commenced offering cab, bus & train booking services.
+What makes our story even stronger is the performance of our newly launched segments, like myBiz—our comprehensive business travel suite and myPartner—an exclusive platform for travel agents. Entering the Gulf market is our latest feat, where we offer power-packed deals on flights & hotels.
+
+
+        </p>
+        <a href="about.php" class="btn">Read more</a>
+    </div>
+</section>
+<!-- home about section ends-->
+
+<!--home packages section starts-->
+<section class="home-packages">
+    <h1 class="heading-title"> our packages </h1>
+    <div class="box-container">
+        <div class="box">
+            <div class="image">
+                <img src="images/img-1.jpg "alt="">
+            </div>
+            <div class="content">
+                <h3>Darjeeling-West Bengal</h3>
+                <p>Darjeeling is a popular town in West Bengal state and is nestled gorgeously in the Himalayan foothills.It is a famous hill station which is popular for its stunning lush green tea gardens and breathtaking landscape</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+
+        <div class="box">
+            <div class="image">
+                <img src="images/img-2.jpg " alt="">
+            </div>
+            <div class="content">
+                <h3>Dharamshala-Himachal</h3>
+                <p>Dharamshala is famously referred as the Scotland of India, which features snowcapped mountain peaks and stunning scenic views. The place is enhanced with oak and conifer trees and surrounded by snow-covered mountains on three sides and valley on fourth side.</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/img-3.jpg " alt="">
+            </div>
+            <div class="content">
+                <h3>Goa</h3>
+                <p>The amazing city of Goa is full of scenic beaches, outstanding churches and incredible forts. The land of sand, sea surf and sun is not just beautiful but romantic as well. This is why; Goa holiday packages for couples are always in demand. The endless fun opportunities in this magical city are meant to offer endless fun.</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+    </div>
+    <div class="load-more"><a href="package.php" class="btn">load more</a></div>
+</section>
+<!-- home packages section ends-->
+
+<!--home offer section starts-->
+<section class="home-offer">
+    <div class="content">
+        <h3>upto 50% off</h3>
+        <p>The best for less. Don't delay—sales today! Keep calm and buy up. It's the biggest sale of the year! Everything you need—on a budget. Enjoy the best rates</p>
+        <a href="book.php" class="btn">book now</a>
+        </div>
+</section>
+<!--home offer section ends-->
+
+<!--footer section starts-->
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>quick links</h3>
+
+        <a href="index.php"><i class="fas fa-angle-right"></i>home</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+            <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+            <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+
+        <a href="a"><i class="fas fa-angle-right"></i>Ask Questions</a>
+        <a href="a"><i class="fas fa-angle-right"></i>about us</a>    
+        <a href="a"><i class="fas fa-angle-right"></i>privacy policies</a>
+        <a href="a"><i class="fas fa-angle-right"></i>terms of use</a>
+        
+        </div>
+
+
+        <div class="box">
+            <h3>Contact info</h3>
+
+        <a href="a"><i class="fas fa-phone"></i>+123-456-7890</a>
+        <a href="a"><i class="fas fa-phone"></i>+121-456-7890</a>
+        <a href="a"><i class="fas fa-envelope"></i>tours&travels@gmail.com</a>
+        <a href="a"><i class="fas fa-map"></i>Lucknow,India -226018</a>
+        </div>
+        
+        <div class="box">
+            <h3>Follow us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+            <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+
+
+        </div>
+    </div>
+
+    <div class="credit"> created by <span>Ms.Iqra Khan</span> |All rights reserved! </div>
+
+
+
+
+
+</section>
+
+
+
+
+<!--footer section ends-->
+
+
+<!-- swiper js link-->
+ <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<!-- custom js file link-->
+<script src="js/script.js"></script>
+</body>
+</html>
---
+++ package.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>about</title>
+     <!-- swiper css link-->
+     <link  rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+<!--font awesome cdn link-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+<!-- custom css file link-->
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!-- header section starts-->
+     <section class="header">
+        <a href="home.php" class="logo">Tours & Travels</a>
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="package.php">package</a>
+            <a href="book.php">book</a>
+        </nav>
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+     </section>
+     <!-- header section ends -->
+
+     <div class="heading" style="background:url(images/header-bg-2.jpg) no-repeat">
+        <h1>packages</h1>
+     </div>
+     <!--packages section starts-->
+     <section class="packages">
+        <h1 class="heading-title">Top destinations</h1>
+        <div class="box-container">
+        <div class="box">
+                <div class="image">
+                    <img src="images/img-1.jpg" alt="">
+                </div>
+            <div class="content">
+                <h3>Darjeeling</h3>
+                <p> With endless places to explore, Darjeeling is one such destination which tops the list when it comes to holidaying in India. It is one of the best travel destinationsin West Bengal state of India. You too can plan Darjeeling tour with amazing Darjeeling holiday packages from EaseMyTrip.com</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+        <div class="box">
+                <div class="image">
+                    <img src="images/img-2.jpg" alt="">
+                </div>
+            <div class="content">
+                <h3>Dharamshala</h3>
+                <p>The incredible beauty of Dharamshala is the greatest reason behind the demands of Dharamshala tour packages. On your Dharamshala tour, you may enjoy the excellent views from every vantage point.</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+        <div class="box">
+                <div class="image">
+                    <img src="images/img-3.jpg" alt="">
+                </div>
+            <div class="content">
+                <h3>Goa</h3>
+                <p>From honeymooners to nightlife enthusiasts and from fun freaks to serenity seekers, Goa offers something to everyone. So, family holiday packages in Goa are also very popular.</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+        <div class="box">
+                <div class="image">
+                    <img src="images/img-4.jpg" alt="">
+                </div>
+            <div class="content">
+                <h3>Kerela</h3>
+                <p>Explore everything of this state right from its beautiful cities to hill-stations by booking Kerala holiday packages available at EaseMyTrip.com. Here, you can choose any kind of Kerala tour packages from the list and plan a Kerala vacation accordingly!</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+        <div class="box">
+                <div class="image">
+                    <img src="images/img-5.jpg" alt="">
+                </div>
+            <div class="content">
+                <h3>Switzerland</h3>
+                <p>There are many hot spots in Switzerland and some of them are Swiss Alps, Chillon Castle, Lake Geneva, Lake Zurich, Matterhorn, Jungfrau etc, these are some wonderful places which attract tourists from worldwide places. Plan your Switzerland tour and enjoy in the beautiful destination.</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+        <div class="box">
+                <div class="image">
+                    <img src="images/img-6.jpg" alt="">
+                </div>
+            <div class="content">
+                <h3>Paris</h3>
+                <p>Paris is famous for its wide variety of cuisines. Explore Paris and its amazing destinations and its cuisines and have some quality time in Paris with the best holiday packages, visit EaseMyTrip.com for the effective tour packages.</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+        <div class="box">
+                <div class="image">
+                    <img src="images/img-7.jpg" alt="">
+                </div>
+            <div class="content">
+                <h3>Sikkim</h3>
+                <p>Sikkim is scenic and verdant with clean crisp air, deep blue mountain lakes, gorgeous Buddhist monasteries and hillsides ablaze with rhododendrons against a backdrop of snow-clad mountains. This picturesque state is located in the northwest India and bordered by Bhutan, Tibet and Nepal.</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+        <div class="box">
+                <div class="image">
+                    <img src="images/img-8.jpg" alt="">
+                </div>
+            <div class="content">
+                <h3>Leh</h3>
+                <p> There are several tourist attractions in Leh which one can visit on Leh tour and some of them are Shanti Stupa, Leh Palace, War Museum, Chamba Temple, Sankar Gompa and Zorawar Fort. Apart from sightseeing in Leh one can indulge in several adventure activities too!</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+        <div class="box">
+                <div class="image">
+                    <img src="images/img-9.jpg" alt="">
+                </div>
+            <div class="content">
+                <h3>Ladakh</h3>
+                <p>This place is well known for its mountains, high passes, scenic aura and its culture. This place is popular among the adventure lovers because of several activities which one can do on Ladakh tour, some of them are white water rafting, trekking, and biking at Khardung La Pass, Camping and Paragliding.</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+        <div class="box">
+                <div class="image">
+                    <img src="images/img-10.jpg" alt="">
+                </div>
+            <div class="content">
+                <h3>Rajasthan</h3>
+                <p>This spectacular state is renowned for its glory and valor of erstwhile kings, colorful and rich cultural heritage. This state offers abundant opportunities for vacationers to explore, be it history, romantic getaways, sightseeing or wedding destination.!</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+        <div class="box">
+                <div class="image">
+                    <img src="images/img-11.jpg" alt="">
+                </div>
+            <div class="content">
+                <h3>Gangtok</h3>
+                <p>Situated at an elevation of 1,650 meters, the city is home to different ethnicities such as Nepalis, Lepchas and Bhutia. The city creates a perfect atmosphere to enjoy some beautiful moments in seclusion. It is one of the top 10 holiday destinations of North East India. Gangtok tour packages are in demand among the solo travelers seeking some of solitude. I</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+        <div class="box">
+                <div class="image">
+                    <img src="images/img-12.jpg" alt="">
+                </div>
+            <div class="content">
+                <h3>Kashmir</h3>
+                <p>The place enjoys diverse wildlife, snow-capped mountains, picturesque valleys, charming landscapes, flowery gardens, traditional houseboats and much more. Known with beautiful names like 'Alps of East', 'Kingdom of Heaven' and 'Paradise on Earth', Kashmir is an awesome destination for honeymooners but others as well visit this wonderful destination for getting connected to the beauty of nature.</p>
+                <a href="book.php" class="btn">book now</a>
+            </div>
+        </div>
+
+
+
+        </div>
+
+
+
+
+        <div class="load-more"><span class="btn">load more</span></div>
+     </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     <!--package section ends-->
+
+
+
+
+
+
+
+
+<!--footer section starts-->
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>quick links</h3>
+
+        <a href="home.php"><i class="fas fa-angle-right"></i>home</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+            <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+            <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+
+        <a href="a"><i class="fas fa-angle-right"></i>Ask Questions</a>
+        <a href="a"><i class="fas fa-angle-right"></i>about us</a>    
+        <a href="a"><i class="fas fa-angle-right"></i>privacy policies</a>
+        <a href="a"><i class="fas fa-angle-right"></i>terms of use</a>
+        
+        </div>
+
+
+        <div class="box">
+            <h3>Contact info</h3>
+
+        <a href="a"><i class="fas fa-phone"></i>+123-456-7890</a>
+        <a href="a"><i class="fas fa-phone"></i>+121-456-7890</a>
+        <a href="a"><i class="fas fa-envelope"></i>tours&travels@gmail.com</a>
+        <a href="a"><i class="fas fa-map"></i>Lucknow,India -226018</a>
+        </div>
+        
+        <div class="box">
+            <h3>Follow us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+            <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+
+
+        </div>
+    </div>
+
+    <div class="credit"> created by <span>Ms.Iqra Khan</span> |All rights reserved! </div>
+
+
+
+
+
+</section>
+
+
+
+
+<!--footer section ends-->
+
+
+<!-- swiper js link-->
+ <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<!-- custom js file link-->
+<script src="js/script.js"></script>
+</body>
+</html>
